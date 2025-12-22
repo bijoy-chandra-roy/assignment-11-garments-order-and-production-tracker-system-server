2ebc2220476f2ae9fc263e86e15c433fc3929454
Bijoy Chandra Roy <142526046+bijoy-chandra-roy@users.noreply.github.com>
2025-12-22 23:25:59 +0600

    key conversion

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const admin = require("firebase-admin");
-const serviceAccount = require("./haystack-firebase-adminsdk.json");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
 const port = process.env.PORT || 3000
 
 admin.initializeApp({
@@ -429,8 +430,8 @@ async function run() {
             });
         });
 
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
     }
 }
